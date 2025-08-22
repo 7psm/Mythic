efd8fe0114d4cee1539337ef6d757b371dea9311
Evannn <contact.imad.t@gmail.com>
2025-08-22 12:48:48 +0200

    test api

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -142,7 +142,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         const result = await response.json();
-        if (result.success) window.location.href = "/confirmation.html";
+        if (result.success) window.location.href = "/pages/confirmation.html";
         else console.error("Erreur serveur:", result.message);
 
       } catch (err) {
